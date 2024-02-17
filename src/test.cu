649d610844fc7cc3c5fd5c0a66eba15729855d67
Jinyang Li <jinyang@cs.nyu.edu>
2024-02-17 00:28:14 -0500

    add lab

---
+++ src/test.cu
@@ -0,0 +1,213 @@
+#include <getopt.h>
+
+#include "utils/tensor.cuh"
+#include "ops/op_mm.cuh"
+#include "ops/op_elemwise.cuh"
+#include "ops/op_reduction.cuh"
+
+unsigned long long randgen_seed = 0;
+
+void test_matmul(int m, int n, int k, bool on_gpu)
+{
+    Tensor<float> A{m, k, on_gpu};
+    op_uniform_init(A);
+
+    Tensor<float> B{k, n, on_gpu};
+    op_uniform_init(B);
+
+    Tensor<float> C{m, n, on_gpu};
+    op_mm(A, B, C);
+
+    Tensor<float> C2{n, m, on_gpu};
+    op_mm(B.transpose(), A.transpose(), C2);
+    assert(op_allclose(C2.transpose(), C)); // test transpose
+
+    std::cout << "matmul passed..." << std::endl;
+}
+
+void test_elemwise(int m, int n, bool on_gpu)
+{
+    
+    Tensor<float> X{m, n, on_gpu};
+    op_const_init(X, 2.0);
+
+    Tensor<float> Y{m, n, on_gpu};
+    op_const_init(Y, 3.0);
+
+    Tensor<float> Z{m, n, on_gpu};
+    op_add(X, Y, Z);
+
+    Tensor<float> Zref{m, n, false};
+    op_const_init(Zref, 5.0);
+    assert(op_allclose(Z, Zref));
+
+    Tensor<float> Y2{1, n, on_gpu};
+    op_const_init(Y2, 3.0);
+    op_add(X, Y2, Z); //test broadcasting
+    assert(op_allclose(Z, Zref));
+
+    op_add<float>(X, 3.0, Z);
+    assert(op_allclose(Z, Zref));
+
+    std::cout << "op_add passed..." << std::endl;
+
+    op_multiply(X, Y, Z);
+
+    op_const_init(Zref, 6.0);
+    assert(op_allclose(Z, Zref));
+
+    op_multiply(X, Y2, Z);
+    assert(op_allclose(Z, Zref));
+
+    op_multiply<float>(X, 3.0, Z);
+    assert(op_allclose(Z, Zref));
+
+    std::cout << "op_multiply passed..." << std::endl;
+
+    float lr = 0.02;
+    Tensor<float> A{m, n, on_gpu};
+    op_uniform_init(A);
+    Tensor<float> A_host = A.toHost();
+
+    Tensor<float> dA{m, n, on_gpu};
+    op_uniform_init(dA);
+    Tensor<float> dA_host = dA.toHost();
+
+    Tensor<float> Aref{m, n, false};
+    for (int i = 0; i < Aref.h; i++)
+    {
+        for (int j = 0; j < Aref.w; j++)
+        {
+          Index(Aref, i, j) = Index(A_host, i, j) - lr * Index(dA_host, i, j);
+        }
+    }
+    op_sgd(A, dA, A, lr);
+    assert(op_allclose(A, Aref));
+
+    std::cout << "op_sgd passed..." << std::endl;
+
+}
+
+void 
+test_reduction(int m, int n, bool on_gpu)
+{
+    Tensor<int> X_host{m, n};
+    op_const_init(X_host, 0);
+
+    int reduce_sum = m>n?n:m;
+    for (int i = 0; i < X_host.h; i++) 
+    {
+        if (i >= X_host.w) {
+            break;
+        }
+        Index(X_host, i, i) = 1;
+    }
+
+    Tensor<int> X;
+    if (on_gpu) {
+        X = X_host.toDevice();
+    } else {
+        X = X_host;
+    } 
+    Tensor<int> Y{1,n, on_gpu};
+    op_sum(X,Y);
+
+    Tensor<int> Yref{1,n};
+    op_const_init(Yref,0);
+    for (int j=0; j< reduce_sum; j++) {
+        Index(Yref, 0, j) = 1;
+    }
+    Tensor<int> Y_host = Y.toHost();
+    assert(op_allclose(Y, Yref));
+
+    Tensor<int> Y1{1,1, on_gpu};
+    op_sum(Y, Y1);
+    Tensor<int> Y1_host = Y1.toHost();
+    assert(Index(Y1_host,0,0) == reduce_sum);
+
+    op_const_init(X, 1);
+    op_sum(X, Y);
+    op_const_init(Yref, X.h);
+    assert(op_allclose(Y, Yref));
+    
+    Tensor<int> YY{m, 1, on_gpu};
+    op_sum(X, YY);
+    Tensor<int> YYref{m, 1};
+    op_const_init(YYref, n);
+    op_sum(YY, Y1);
+    Y1_host = Y1.toHost();
+    assert(Index(Y1_host, 0, 0) == m*n);
+
+    std::cout << "op_sum passed..." << std::endl;
+
+    //try to create an A matrix whose last column has the biggest value
+    Tensor<float> A{m, n, on_gpu};
+    op_uniform_init<float>(A, 0.0, 1.0);
+    auto AA = A.slice(0, A.h, A.w-1, A.w);
+    op_add<float>(AA, 10.0, AA);
+
+    Tensor<int> ind{m, 1, on_gpu};
+    op_argmax(A, ind);
+    Tensor<int> indref{m, 1};
+    op_const_init(indref, n-1);
+    assert(op_allclose(ind, indref));
+}
+
+void 
+test_views()
+{
+    Tensor<float> A{5, 5};
+    for (int i = 0; i < A.h; i++) {
+        for (int j = 0; j < A.w; j++) {
+            Index(A, i, j) = i*A.w+j;
+        }
+    }
+    auto B = A.slice(1,3,1,3);
+    assert(Index(B, 0, 0) == 6);
+    assert(Index(B, 0, 1) == 7);
+    assert(Index(B, 1, 0) == 11);
+    assert(Index(B, 1, 1) == 12);
+    auto C = B.transpose();
+    assert(Index(C, 0, 0) == 6);
+    assert(Index(C, 0, 1) == 11);
+    assert(Index(C, 1, 0) == 7);
+    assert(Index(C, 1, 1) == 12);
+    std::cout << "slice passed..." << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    bool test_gpu = true;
+    int test_m = 335, test_n = 587, test_k= 699;
+
+    for (;;)
+    {
+        switch (getopt(argc, argv, "s:ch:l:b:e:"))
+        {
+        case 's':
+            randgen_seed = atoll(optarg);
+            continue;
+        case 'c': //cpu testing only
+            test_gpu = false;
+            continue;
+        case 'm':
+            test_m = atoi(optarg);
+            continue;
+        case 'n':
+            test_n = atoi(optarg);
+            continue;
+        case 'k':
+            test_k = atoi(optarg);
+            continue;
+        case -1:
+            break;
+        }
+        break;
+    }
+    test_views();
+    test_elemwise(test_m, test_n, test_gpu);
+    test_matmul(test_m, test_n, test_k, test_gpu);
+    test_reduction(test_m, test_n, test_gpu);
+    std::cout << "All tests completed successfully!" << std::endl;
+    return 0;
+}
---
+++ src/train_mlp.cu
@@ -0,0 +1,118 @@
+#include <getopt.h>
+
+#include "modules/mlp.cuh"
+#include "modules/linear.cuh"
+#include "modules/sgd.cuh"
+
+#include "utils/dataset_mnist.hh"
+#include "ops/op_elemwise.cuh"
+#include "ops/op_cross_entropy.cuh"
+
+unsigned long long randgen_seed = 0;
+
+int correct(const Tensor<float> &logits, const Tensor<char> &targets) 
+{
+    assert(targets.w == 1);
+    Tensor<int> predictions{targets.h, targets.w};
+    op_argmax(logits, predictions);
+    Tensor<int> correct_preds{targets.h, targets.w};
+    op_equal(predictions, targets, correct_preds);
+    Tensor<int> sum_correct{1,1};
+    op_sum(correct_preds, sum_correct);
+    return Index(sum_correct, 0, 0);
+}
+
+void train(int epochs, int batch_size, int hidden_dim, int n_layers, bool on_gpu)
+{
+    MNIST mnist_train{"../data/MNIST/raw", MNIST::Mode::kTrain};
+    MNIST mnist_test{"../data/MNIST/raw", MNIST::Mode::kTest};
+    
+    std::cout << "# of data points=" << mnist_train.images.h << " feature size=" << mnist_train.images.w << std::endl;
+    std::cout << "all datapoints mean=" << mnist_train.images.mean() << std::endl;
+
+    std::vector<int> layer_dims;
+    for (int i = 0; i < n_layers - 1; i++)
+    {
+        layer_dims.push_back(hidden_dim);
+    }
+    layer_dims.push_back(10); // last layer's out dimension is always 10 (# of digits)
+
+    MLP<float> mlp{batch_size, MNIST::kImageRows * MNIST::kImageColumns, layer_dims, false};
+    mlp.init();
+    SGD<float> sgd{mlp.parameters(), 0.01};
+
+    Tensor<float> logits{batch_size, 10};
+    Tensor<float> d_logits{batch_size, 10};
+    Tensor<float> d_input_images{batch_size, mnist_train.images.w};
+    float loss;
+    int num_batches, total_correct;
+    float total_loss;
+    for (int i = 0; i < epochs; i++)
+    {
+        num_batches = 0;
+        total_loss = 0.0;
+        total_correct = 0;
+        for (int b = 0; b < mnist_train.images.h / batch_size; b++)
+        {
+            if ((b + 1) * batch_size >= mnist_train.images.h)
+            {
+                break;
+            }
+            num_batches++;
+            Tensor<float> input_images = mnist_train.images.slice(b * batch_size, (b + 1) * batch_size, 0, mnist_train.images.w);
+            Tensor<float> debug_t = input_images.slice(0, 1, 0, mnist_train.images.w);
+            Tensor<char> targets = mnist_train.targets.slice(b * batch_size, (b+1)*batch_size, 0, mnist_train.targets.w);
+            mlp.forward(input_images, logits);
+
+            loss = op_cross_entropy_loss(logits, targets, d_logits);
+            total_loss += loss;
+            total_correct += correct(logits, targets);
+            
+            mlp.backward(input_images, d_logits, d_input_images);
+            sgd.step();
+        }
+        std::cout << "epoch=" << i << " loss=" << total_loss/num_batches 
+            << " accuracy=" << total_correct/(float)(num_batches*batch_size)
+            << " num_batches=" << num_batches 
+            << std::endl;
+
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    bool on_gpu = false;
+    int hidden_dim = 16;
+    int n_layers = 2;
+    int batch_size = 32;
+    int num_epochs = 10;
+
+    for (;;)
+    {
+        switch (getopt(argc, argv, "s:g:h:l:b:e:"))
+        {
+        case 's':
+            randgen_seed = atoll(optarg);
+            continue;
+        case 'g':
+            on_gpu = true;
+            continue;
+        case 'h':
+            hidden_dim = atoi(optarg);
+            continue;
+        case 'l':
+            n_layers = atoi(optarg);
+            continue;
+        case 'b':
+            batch_size = atoi(optarg);
+            continue;
+        case 'e':
+            num_epochs = atoi(optarg);
+            continue;
+        case -1:
+            break;
+        }
+        break;
+    }
+    train(num_epochs, batch_size, hidden_dim, n_layers, on_gpu);
+}
